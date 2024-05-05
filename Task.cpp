f90f77ef38c8887799310e0f10e3954f5c9510d5
Sergei <w.silton.it@gmail.com>
2024-05-05 19:18:08 +0300

    Update solution

--- Task.cpp
+++ Task.cpp
@@ -13,37 +13,37 @@ int main() {
 	time = 16;
 	double price = 0.8;
 	Bus bus(start, end, time, price);
-	move = bus;
-        std::cout << move << std::endl;
+	Move& move1 = bus;
+        std::cout << move1 << std::endl;
 
 	start._x = 1; start._y = 7;                                                                                                                                                  
         end._x = 5; end._y = 183;      
         time = 40;                                                                                                                                                
         price = 0.8;
 	Metro metro(start, end, time, price);
-	move = metro;
-        std::cout << move << std::endl;
+	Move& move2 = metro;
+        std::cout << move2 << std::endl;
 
 	start._x = 1; start._y = 47;
         end._x = 35; end._y = 143;
         time = 30;                             
         price = 0.95;                                                                                                                                                                    	Trolley trolly(start, end, time, price);
-	move = trolly;
-        std::cout << move << std::endl;
+	Move& move3 = trolly;
+        std::cout << move3 << std::endl;
 
 	start._x = 1; start._y = 7;                                                                                                                                                     
         end._x = 59; end._y = 7183;
         time = 12;                             
         price = 1.8;                                                                                                                                                                    
         Taxi taxi(start, end, time, price);
-	move = taxi;
-        std::cout << move << std::endl;
+	Move& move4 = taxi;
+        std::cout << move4 << std::endl;
 
 	start._x = 15; start._y = 7;                                                                                                                                                    
         end._x = 85; end._y = 1883;                                                                                                                                                       	time = 10;                             
         price = 9.8;                                                                                                                                                                    
 	char* path = new char[17]{'h', 't', 't', 'p', ':', '/', '/', 'd', 'r', 'i', 'v', 'e', '.', 'c', 'o', 'm' };
 	Car car(start, end, time, price, path);
-	move = car;
-        std::cout << move << std::endl;
+	Move& move5 = car;
+        std::cout << move5 << std::endl;
 }
