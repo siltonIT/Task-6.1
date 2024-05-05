e2de8887329b066aa6d1bbf7275dae33d5b67eb1
Sergei <w.silton.it@gmail.com>
2024-05-05 18:53:53 +0300

    Create solution

--- Task.cpp
+++ Task.cpp
@@ -1,1 +1,49 @@
 #include "Task.h"
+
+int main() {
+	Point start(1, 2);
+	Point end(5, 4);
+	size_t time = 50;
+	Walk walk(start, end, time);
+	Move& move = walk;
+	std::cout << move << std::endl;
+
+	start._x = 1.2; start._y = 78;
+	end._x = 25; end._y = 185.3;
+	time = 16;
+	double price = 0.8;
+	Bus bus(start, end, time, price);
+	move = bus;
+        std::cout << move << std::endl;
+
+	start._x = 1; start._y = 7;                                                                                                                                                  
+        end._x = 5; end._y = 183;      
+        time = 40;                                                                                                                                                
+        price = 0.8;
+	Metro metro(start, end, time, price);
+	move = metro;
+        std::cout << move << std::endl;
+
+	start._x = 1; start._y = 47;
+        end._x = 35; end._y = 143;
+        time = 30;                             
+        price = 0.95;                                                                                                                                                                    	Trolley trolly(start, end, time, price);
+	move = trolly;
+        std::cout << move << std::endl;
+
+	start._x = 1; start._y = 7;                                                                                                                                                     
+        end._x = 59; end._y = 7183;
+        time = 12;                             
+        price = 1.8;                                                                                                                                                                    
+        Taxi taxi(start, end, time, price);
+	move = taxi;
+        std::cout << move << std::endl;
+
+	start._x = 15; start._y = 7;                                                                                                                                                    
+        end._x = 85; end._y = 1883;                                                                                                                                                       	time = 10;                             
+        price = 9.8;                                                                                                                                                                    
+	char* path = new char[17]{'h', 't', 't', 'p', ':', '/', '/', 'd', 'r', 'i', 'v', 'e', '.', 'c', 'o', 'm' };
+	Car car(start, end, time, price, path);
+	move = car;
+        std::cout << move << std::endl;
+}
--- Task.h
+++ Task.h
@@ -1,1 +1,11 @@
+#pragma once
 
+#include <iostream>
+#include "Walk.h"
+#include "Bus.h"
+#include "Taxi.h"
+#include "Trolley.h"
+#include "Car.h"
+#include "Metro.h"
+
+int main();
